a6ddbf77fe6584f22d143ebee076658ae8902b21
Cheesyburrito30 <pvernon30@gmail.com>
2017-11-02 10:14:25 -0400

    started classes

--- classes/Program.cs
+++ classes/Program.cs
@@ -2,11 +2,56 @@
 
 namespace classes
 {
+    // Everything in C# is made around classes.
+    // classes are groups of related methods and variables
+    // to use classes, you create a new instance of the class (like we did in
+    // the functions solution) and you can then run use the methods and classes
+    // from the class 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this is us creating the new instance of the class Person.
+            // now throughout our class Program, we can use Person's methods
+            // and variables. By calling the class then the method
+            // i.e.: 'person.describe()'
+            Person person;
+            string first;
+            string last;
+            int age;
+            Console.WriteLine("Type in a first name");
+            first = Console.ReadLine();
+            Console.WriteLine("Type in a last name");
+            last = Console.ReadLine();
+            Console.WriteLine("Type in their age");
+            age = int.Parse(Console.ReadLine());
+            person = new Person(first, last, age);
+            Console.WriteLine(person.Describe());
         }
+    }
+
+    // Normally classes will be created in their own files so it's easier to 
+    // see the structure of the program. good practice is to have your variables
+    // be private because if they are public they're accessable by everything,
+    // which isn't fantastic. Instead you make them accessible through a method.
+
+    class Person
+    {
+        private string fName;
+        private string lName;
+        private int age;
+
+        public Person(string fname, string lname, int age)
+        {
+            this.fName = fname;
+            this.lName = lname;
+            this.age = age;
+        }
+        public string Describe()
+        {
+            return "This Person is " + fName + " " + lName + " and they are " +
+                age + " years old.";
+        }
+
     }
 }
